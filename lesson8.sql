c19a773c898aa818928e6547e01f1ccd8112a61d
hmkuklinski <hmkuklinski@gmail.com>
2024-06-26 23:29:03 -0400

    review/practice

---
+++ lesson8.sql
@@ -0,0 +1,23 @@
+-- unique cuisine
+SELECT DISTINCT cuisine
+FROM restaurants;
+
+-- chinese restaurants
+SELECT *
+FROM restaurants
+WHERE cuisine= 'Chinese'
+
+-- Italian restaurants
+SELECT *
+FROM restaurants
+WHERE cuisine= 'Italian'
+
+-- From Greenpoint NGH:
+SELECT * 
+FROM restaurants
+WHERE neighborhood = 'Greenpoint'
+
+-- Cheapest restaurants:
+SELECT *
+FROM restaurants
+WHERE price = '$'
